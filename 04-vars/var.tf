37375022a2eea5ae746e452a32c1a9bace5d197c
purna <purnaraomba@gmail.com>
2023-09-21 20:37:54 +0530

    RoboShop Update

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -1,34 +1,34 @@
-variable "sample" {
-  default = 200
-}
-
-output "sample" {
-  value = var.sample
-}
-
-variable "sample1"{}
-
-output "sample1" {
-  value = var.sample1
-}
-
-variable "sample2" {}
-
-output "sample2" {
-  value = var.sample2
-}
-
-variable "sample3" {}
-
-output "test" {
-  value = var.sample3
-}
-
-variable "input" {}
-
-output "input" {
-  value = var.input
-}
+#variable "sample" {
+#  default = 200
+#}
+#
+#output "sample" {
+#  value = var.sample
+#}
+#
+#variable "sample1"{}
+#
+#output "sample1" {
+#  value = var.sample1
+#}
+#
+#variable "sample2" {}
+#
+#output "sample2" {
+#  value = var.sample2
+#}
+#
+#variable "sample3" {}
+#
+#output "test" {
+#  value = var.sample3
+#}
+#
+#variable "input" {}
+#
+#output "input" {
+#  value = var.input
+#}
 
 variable "sample4" {
   default = [10,"apple"]
