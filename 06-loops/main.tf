089dbcae9c45f338b8dd5d6d53802b9514c81a0c
purna <purnaraomba@gmail.com>
2023-09-23 20:22:20 +0530

    RoboShop Update

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -13,3 +13,21 @@ variable "d2" {
 resource "null_resource" "d2" {
   count = length(var.d2)
 }
+
+variable "d3" {
+  default = {
+    apple = {
+      name = apple
+      count = 3
+    }
+    banana = {
+      name = apple
+      count = 3
+    }
+  }
+
+}
+
+resource "null_resource" "d3" {
+  each_value = var.d3
+}
