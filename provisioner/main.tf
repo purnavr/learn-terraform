45dcb84f2fc11ec2b15e7e52b4af718c22c3cb64
purna <purnaraomba@gmail.com>
2023-09-30 20:22:54 +0530

    RoboShop Update

---
+++ provisioner/main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = "sg-0497e25cd969a429f"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "web" {
+  connection {
+    user     = "root"
+    password = DevOps321
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo hello"
+    ]
+  }
+}
