071d4db6682a584e6ff3bcae41150e4728c75822
purna <purnaraomba@gmail.com>
2023-09-30 19:45:37 +0530

    RoboShop Update

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,3 +1,15 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
 
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
 
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 
