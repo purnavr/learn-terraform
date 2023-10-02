ef32c3eef993eb2b4fbbc278e622e41fdfde81dc
purna <purnaraomba@gmail.com>
2023-10-02 18:41:20 +0530

    Roboshop Update

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -5,13 +5,13 @@ data "aws_ami" "ami" {
 }
 
 
-resource "aws_instance" "instances" {
+resource "aws_instance" "frontend" {
   for_each               = var.instances
-  ami                    = data.aws_ami.ami.image_id
-  instance_type          = each.value["type"]
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-  tags = {
-    Name = each.value["name"]
+  ami                    = data.aws_ami.ami.id
+  instance_type          = each.value[type]
+  vpc_security_group_ids = ["sg-0497e25cd969a429f"]
+  tags                   = {
+    Name = each.value[name]
   }
 }
 
@@ -19,58 +19,19 @@ variable "instances" {
   default = {
     catalogue = {
       name = "catalogue"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     user = {
       name = "user"
-      type = "t3.small"
+      type = "t2.micro"
     }
   }
 }
 
-output "ec2" {
-  value = [for k, v in aws_instance.instances : "${k} - ${v.public_ip}"]
+output "public-ip" {
+  value = aws_instance.frontend.*.public_ip
 }
 
-## Immature code
-//variable "names" {
-//  default = [ "catalogue", "user"]
-//}
-//
-//variable "types" {
-//  default = ["t3.micro", "t3.small"]
-//}
-//
-//resource "aws_instance" "instances" {
-//  count = length(var.names)
-//  ami                    = data.aws_ami.ami.image_id
-//  instance_type          = var.types[count.index]
-//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-//  tags = {
-//    Name = var.names[count.index]
-//  }
-//}
-
-## Little mature code
-//variable "demo" {
-//  default = [
-//    {
-//      name = "catalogue"
-//      type = "t3.micro"
-//    },
-//    {
-//      name = "user"
-//      type = "t3.small"
-//    }
-//  ]
-//}
-//
-//resource "aws_instance" "instances" {
-//  count                  = length(var.demo)
-//  ami                    = data.aws_ami.ami.image_id
-//  instance_type          = var.demo[count.index]["type"]
-//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-//  tags = {
-//    Name = var.demo[count.index]["name"]
-//  }
-//}
+output "pub_ip" {
+  value = [for k, v in aws_instance.frontend : "${k} - ${v.public_ip}"]
+}
