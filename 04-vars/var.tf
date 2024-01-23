5b38eb6ad191ca92feea144ffdcc202122f9a8ae
purna <purnaraomba@gmail.com>
2024-01-23 18:10:46 +0530

    Roboshop Update

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -24,32 +24,32 @@ output "test" {
   value = var.sample3
 }
 
-#variable "input" {}
-#
-#output "input" {
-#  value = var.input
-#}
-#
-#variable "sample4" {
-#  default = [10, "apple"]
-#}
-#
-#output "sample4" {
-#  value = var.sample4[1]
-#}
-#
-#variable "sample5" {
-#  default = {
-#    fruits = {
-#      apple   = 5
-#      oranges = 5
-#      banana  = true
-#    }
-#  }
-#}
-#
-#output "sample5" {
-#  value = var.sample5.fruits.banana
-#}
+variable "input" {}
+
+output "input" {
+  value = var.input
+}
+
+variable "sample4" {
+  default = [10, "apple"]
+}
+
+output "sample4" {
+  value = var.sample4[1]
+}
+
+variable "sample5" {
+  default = {
+    fruits = {
+      apple   = 5
+      oranges = 5
+      banana  = true
+    }
+  }
+}
+
+output "sample5" {
+  value = var.sample5.fruits.banana
+}
 
 
