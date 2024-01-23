fad5bdb7b87dffbc1e030b82db8e03b8018878b3
purna <purnaraomba@gmail.com>
2024-01-23 17:45:32 +0530

    Roboshop Update

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -1,23 +1,23 @@
-#variable "sample" {
-#  default = 200
-#}
-#
-#output "sample" {
-#  value = var.sample
-#}
-#
-#variable "sample1"{}
-#
-#output "sample1" {
-#  value = var.sample1
-#}
-#
-#variable "sample2" {}
-#
-#output "sample2" {
-#  value = var.sample2
-#}
-#
+variable "sample" {
+  default = 200
+}
+
+output "sample" {
+  value = var.sample
+}
+
+variable "sample1"{}
+
+output "sample1" {
+  value = var.sample1
+}
+
+variable "sample2" {}
+
+output "sample2" {
+  value = var.sample2
+}
+
 #variable "sample3" {}
 #
 #output "test" {
@@ -29,27 +29,27 @@
 #output "input" {
 #  value = var.input
 #}
-
-variable "sample4" {
-  default = [10, "apple"]
-}
-
-output "sample4" {
-  value = var.sample4[1]
-}
-
-variable "sample5" {
-  default = {
-    fruits = {
-      apple   = 5
-      oranges = 5
-      banana  = true
-    }
-  }
-}
-
-output "sample5" {
-  value = var.sample5.fruits.banana
-}
+#
+#variable "sample4" {
+#  default = [10, "apple"]
+#}
+#
+#output "sample4" {
+#  value = var.sample4[1]
+#}
+#
+#variable "sample5" {
+#  default = {
+#    fruits = {
+#      apple   = 5
+#      oranges = 5
+#      banana  = true
+#    }
+#  }
+#}
+#
+#output "sample5" {
+#  value = var.sample5.fruits.banana
+#}
 
 
