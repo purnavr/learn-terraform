1cde1156008168f6a6b0d5f61dbf757a1dfa073c
purna <purnaraomba@gmail.com>
2024-01-23 18:05:34 +0530

    Roboshop Update

--- 08-conditions/main.tf
+++ 08-conditions/main.tf
@@ -1,14 +1,32 @@
-resource "aws_instance" "test" {
-  ami           = "ami-03265a0778a880afb"
+# data block to get the ami id
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+# resource block to create the ec2 instance, condition with string.
+resource "aws_instance" "sample" {
+  ami           = data.aws_ami.ami.id
   instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
+
+  tags = {
+    Name = "sample"
+  }
 }
 
 variable "instance_type" {}
 
-variable "create_instance" {}
-
-resource "aws_instance" "test1" {
-  count         = tobool(var.create_instance) ? 1 : 0
-  ami           = "ami-03265a0778a880afb"
+# resource block to create the ec2 instance, condition with boolean.
+resource "aws_instance" "sample1" {
+  ami           = data.aws_ami.ami.id
   instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
+
+  count = tobool(var.instance_count) ? 1 : 0
+
+  tags = {
+    Name = "sample1"
+  }
 }
+
+variable "instance_count" {}
