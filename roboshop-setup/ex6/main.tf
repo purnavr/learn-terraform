9263005713f9c77a2e6a797d55ef71a60e5af974
purna <purnaraomba@gmail.com>
2024-01-26 18:50:12 +0530

    Roboshop Update

--- roboshop-setup/ex6/main.tf
+++ roboshop-setup/ex6/main.tf
@@ -9,7 +9,6 @@ resource "aws_spot_instance_request" "ec2" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0497e25cd969a429f"]
-#  wait_for_fulfillment   = true
 
   tags = {
     Name = "ec2"
