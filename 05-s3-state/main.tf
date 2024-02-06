cf935fbff20a3d51b437ef2268ed34d34f120bb0
purna <purnaraomba@gmail.com>
2024-02-06 18:38:59 +0530

    Roboshop Update

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -7,10 +7,10 @@ terraform {
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t2.micro"
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t2.micro"
 
-  tags = {
-    Name = "test"
+  tags                   = {
+    Name = "demo"
   }
-}}
+}
