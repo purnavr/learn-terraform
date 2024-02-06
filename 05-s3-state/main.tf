e7eef2c8ed3e3ba515f900de2a2b7d947f4f180b
purna <purnaraomba@gmail.com>
2024-02-06 18:36:48 +0530

    Roboshop Update

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0a017d8ceb274537d"
+  ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+
   tags                   = {
     Name = "demo"
   }
