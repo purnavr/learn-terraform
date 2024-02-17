6ad16e0e34000bee06066072fb1aa4ced31cce08
purna <purnaraomba@gmail.com>
2024-02-17 15:22:01 +0530

    Roboshop Update

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,12 +1,38 @@
+#resource "aws_spot_instance_request" "node1" {
+#  ami           = "ami-0d3a21488b4d72948"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = ["sg-0497e25cd969a429f"]
+#  wait_for_fulfillment = true
+#
+#  tags = {
+#    Name = "prom-test-server"
+#  }
+#}
+#
+#resource "aws_spot_instance_request" "node2" {
+#  ami           = "ami-0d3a21488b4d72948"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = ["sg-0497e25cd969a429f"]
+#  wait_for_fulfillment = true
+#
+#  tags = {
+#    Name = "prom-test-node"
+#  }
+#}
+
+
+
 resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0d3a21488b4d72948"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0497e25cd969a429f"]
   wait_for_fulfillment = true
+}
 
-  tags = {
-    Name = "prom-test-server"
-  }
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
 }
 
 resource "aws_spot_instance_request" "node2" {
@@ -14,36 +40,10 @@ resource "aws_spot_instance_request" "node2" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0497e25cd969a429f"]
   wait_for_fulfillment = true
-
-  tags = {
-    Name = "prom-test-node"
-  }
 }
 
-
-
-#resource "aws_spot_instance_request" "node1" {
-#  ami           = "ami-0d3a21488b4d72948"
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = ["sg-0497e25cd969a429f"]
-#  wait_for_fulfillment = true
-#}
-#
-#resource "aws_ec2_tag" "node1" {
-#  resource_id = aws_spot_instance_request.node1.spot_instance_id
-#  key         = "Name"
-#  value       = "prom-test-server"
-#}
-#
-#resource "aws_spot_instance_request" "node2" {
-#  ami           = "ami-0d3a21488b4d72948"
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = ["sg-0497e25cd969a429f"]
-#  wait_for_fulfillment = true
-#}
-#
-#resource "aws_ec2_tag" "node2" {
-#  resource_id = aws_spot_instance_request.node2.spot_instance_id
-#  key         = "Name"
-#  value       = "prom-test-node"
-#}
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
+}
