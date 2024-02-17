2c644359df6a4b9bea73ba00b2d2dd690f337d1b
purna <purnaraomba@gmail.com>
2024-02-17 14:48:29 +0530

    Roboshop Update

---
+++ prometheus test servers/main.tf
@@ -0,0 +1,25 @@
+resource "aws_spot_instance_request" "node1" {
+  ami           = "ami-0d3a21488b4d72948"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-0497e25cd969a429f]
+  wait_for_fulfillment = true
+}
+
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
+resource "aws_spot_instance_request" "node2" {
+  ami           = "ami-0d3a21488b4d72948"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-0497e25cd969a429f]
+  wait_for_fulfillment = true
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
+}
