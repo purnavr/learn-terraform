cbbc9f1fd41382360e41330922ff0d087df57d2d
purna <purnaraomba@gmail.com>
2024-02-17 14:54:34 +0530

    Roboshop Update

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0d3a21488b4d72948"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0497e25cd969a429f]
+  vpc_security_group_ids = ["sg-0497e25cd969a429f"]
   wait_for_fulfillment = true
 }
 
@@ -14,7 +14,7 @@ resource "aws_ec2_tag" "node1" {
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0d3a21488b4d72948"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0497e25cd969a429f]
+  vpc_security_group_ids = ["sg-0497e25cd969a429f"]
   wait_for_fulfillment = true
 }
 
